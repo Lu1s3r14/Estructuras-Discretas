852e6e55a1da2d1359214edcd372adc9ff97c4d2
lu1s3r14 <luis.enriqueesca@ciencias.unam.mx>
2025-11-18 19:35:53 -0600

    docs: Proyecto (se agrega los ejemplos de uso)

--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -3,7 +3,8 @@ import Aux
 {- Funcion: frecuencias
    Descripcion: dada una cadena de texto, regresa una lista de tuplas que tiene 
                 el caracter y su frecuencia de aparicion.
-   Uso: frecuencias "AVRAHKADABRA" = [('A',5),('V',1),('R',2),('H',1),('K',1),('D',1),('B',1)]
+   Act5 Ejemplo de uso: 
+   frecuencias "OKAWAIIKOTO" = [('O',3),('K',2),('A',2),('W',1),('I',2),('T',1)]
 -}
 
 frecuencias :: String -> [(Char, Int)]
@@ -12,7 +13,8 @@ frecuencias (x:xs) = (x, 1 + (cuenta x xs)) : (frecuencias (remueve x xs))
 
 {- Funcion: arbolhuff
    Descripcion: crea un arbol de Huffman en base a una cadena de texto recibida.
-   Uso: 
+   Act5 Ejemplo de uso:
+   arbolhuff "OKAWAIIKOTO" =  Node (Node (Node (Node (Node (Node Empty (Hoja 'T')) (Hoja 'W')) (Hoja 'I')) (Hoja 'A')) (Hoja 'K')) (Hoja 'O')
 -}
 
 arbolhuff :: String -> HuffmanTree
@@ -25,7 +27,8 @@ arbolhuff frase
 
 {- Funcion: codificarTexto
    Descripcion: Implementa el algoritmo de codificacion en una cadena de texto
-   Uso: codificarTexto "AVRAHKADABRA" = "1001011000100001100000110000001011"
+   Act5 Ejemplo de uso:
+   codificarTexto "OKAWAIIKOTO" = "10100100001001000100010110000011"
 -}
 
 codificarTexto :: String -> String
@@ -38,7 +41,16 @@ codificarTexto texto
 
 {- Funcion: decodificarTexto
    Descripcion: Implementa el algoritmo para decodificar la cadena de texto 
-   Uso: decodificarTexto 
+   Act5 Ejemplo de uso:
+   [let frase = "OKAWAIIKOTO"]  *Se define la frase*
+   [let arbol = arbolhuff frase]  *Se define el arbol de Huffman en base a la frase*
+   [arbol] = Node (Node (Node (Node (Node (Node Empty (Hoja 'T')) (Hoja 'W')) (Hoja 'I')) (Hoja 'A')) (Hoja 'K')) (Hoja 'O')  *Para ver el arbol creado*
+   [let bitscomprimidos = codificarTexto frase]  *se aplica la codificacion a la frase*
+   [bitscomprimidos] = "10100100001001000100010110000011"   *Para ver la frase codificada*
+   [decodificarTexto arbol bitscomprimidos]  * Aplicar la decodificacion a la frase en base al arbol y los bitscomprimidos*
+   - Al ejecutar este ultimo devuelve la frase ya decodificada,  en este caso es "OKAWAIIKOTO" -
+   - En si este es un ejemplo de como ejecutar los algoritmos juntos (menos el de frecuencias) -
+   - estan entre [] lo que se debe poner tal cual en la terminal para que funcione - 
 -}
 
 decodificarTexto :: HuffmanTree -> String -> String
